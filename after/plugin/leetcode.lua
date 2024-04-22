314994f17eb966c99ef9832f61709c01d93bce02
Ahmed Ammar <ahmed29329@gmail.com>
2024-04-22 11:04:46 +0200

    init

---
+++ after/plugin/leetcode.lua
@@ -0,0 +1,8 @@
+vim.g.leetcode = {
+    language = "java"
+}
+require("leetcode").setup()
+
+require("notify").setup({ 
+ background_colour = "#000000",
+})
---
+++ after/plugin/lspzero.lua
@@ -0,0 +1,11 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- here you can setup the language servers
+require'lspconfig'.phpactor.setup{}
+require'lspconfig'.java_language_server.setup{}
