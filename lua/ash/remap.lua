6db4d8ebd2b8786bbf0f574674b85dfe5772b021
Ahmed Ammar <ahmed29329@gmail.com>
2024-04-25 00:36:17 +0200

    cleanups

--- lua/ash/remap.lua
+++ lua/ash/remap.lua
@@ -24,18 +24,18 @@ vim.keymap.set("n", "<C-o>", ":lua require(\"deepsymbols\").get_symbols()<CR>")
 
 
 -- phpactor
-map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {desc = "Rename"})
-map('n', '<leader>fd', '<cmd>lua vim.lsp.buf.definition()<CR>', {desc = "Go to definition"})
-map('n', '<leader>us', ':lua require(\'telescope.builtin\').lsp_references()<CR>', {desc = "Find usages"})
-map('n', '<leader>im', '<cmd>lua vim.lsp.buf.implementation()<CR>', {desc = "Go to implementation"})
-map('n', '<leader>sh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {desc = "Signature help"})
-map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {desc = "Code actions"})
+map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', { desc = "Rename" })
+map('n', '<leader>fd', '<cmd>lua vim.lsp.buf.definition()<CR>', { desc = "Go to definition" })
+map('n', '<leader>us', ':lua require(\'telescope.builtin\').lsp_references()<CR>', { desc = "Find usages" })
+map('n', '<leader>im', '<cmd>lua vim.lsp.buf.implementation()<CR>', { desc = "Go to implementation" })
+map('n', '<leader>sh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { desc = "Signature help" })
+map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { desc = "Code actions" })
 
 -- Mapping Cmd+[ and Cmd+] for navigation through the jump list
-map('n', '<C-[>', '<C-o>', {desc = "Navigate to previous cursor position"})
-map('n', '<C-]>', '<C-i>', {desc = "Navigate to next cursor position"})
+map('n', '<C-[>', '<C-o>', { desc = "Navigate to previous cursor position" })
+map('n', '<C-]>', '<C-i>', { desc = "Navigate to next cursor position" })
 
 
 
 -- conform (code formatter)
-vim.keymap.set('n', '<leader>l', ':Format<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>l', function() vim.lsp.buf.format() end, { noremap = true, silent = true })
