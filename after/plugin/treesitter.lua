ff3580674888ba5a56f8fa8e9ab9c3a481ebb62a
Ahmed Ammar <ahmed29329@gmail.com>
2024-04-27 03:37:37 +0200

    expand selection

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -19,11 +19,8 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "java", "php", "blade" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
   highlight = {
@@ -31,4 +28,14 @@ require'nvim-treesitter.configs'.setup {
 
     additional_vim_regex_highlighting = false,
   },
+
+  incremental_selection = {
+      enable = true,
+      keymaps = {                       -- mappings for incremental selection (visual mappings)
+        init_selection = 'gnn',         -- maps in normal mode to init the node/scope selection
+        node_incremental = "grn",       -- increment to the upper named parent
+        scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
+        node_decremental = "grm",       -- decrement to the previous node
+      }
+    }
 }
