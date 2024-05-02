9e4f830a63c9ee6a078c4d7faeb0a5421140101d
Ahmed Ammar <ahmed29329@gmail.com>
2024-05-02 18:25:16 +0200

    transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,9 +7,11 @@ auto_dark_mode.setup({
 	set_dark_mode = function()
 		vim.api.nvim_set_option('background', 'dark')
 		vim.cmd('colorscheme catppuccin-mocha')
+        vim.cmd('TransparentEnable')
 	end,
 	set_light_mode = function()
 		vim.api.nvim_set_option('background', 'light')
 		vim.cmd('colorscheme github_light')
+        vim.cmd('TransparentDisable')
 	end,
 })
