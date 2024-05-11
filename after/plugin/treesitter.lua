f9acb4465fdd0e6a9c4da7f09b9438cb64fbfb19
Ahmed Ammar <ahmed29329@gmail.com>
2024-05-11 03:04:46 +0200

    ts format

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,41 +1,42 @@
 -- Laravel Blade
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.blade = {
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = {"src/parser.c"},
-    branch = "main",
-  },
-  filetype = "blade"
+    install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+    filetype = "blade"
 }
 
 vim.filetype.add({
-  pattern = {
-    ['.*%.blade%.php'] = 'blade',
-  },
+    pattern = {
+        ['.*%.blade%.php'] = 'blade',
+    },
 })
 
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "java", "php", "blade" },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "java", "php", "blade" },
 
-  sync_install = false,
+    sync_install = false,
 
-  auto_install = true,
+    auto_install = true,
 
-  highlight = {
-    enable = true,
 
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {
+        enable = true,
 
-  incremental_selection = {
-      enable = true,
-      keymaps = {                       -- mappings for incremental selection (visual mappings)
-        init_selection = 'gnn',         -- maps in normal mode to init the node/scope selection
-        node_incremental = "grn",       -- increment to the upper named parent
-        scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
-        node_decremental = "grm",       -- decrement to the previous node
-      }
+        additional_vim_regex_highlighting = false,
+    },
+
+    incremental_selection = {
+        enable = true,
+        keymaps = {                -- mappings for incremental selection (visual mappings)
+            init_selection = 'gnn', -- maps in normal mode to init the node/scope selection
+            node_incremental = "grn", -- increment to the upper named parent
+            scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
+            node_decremental = "grm", -- decrement to the previous node
+        }
     }
 }
