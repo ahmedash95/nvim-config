58b75f5424bf5ae7301240d299eabb319e7ef04e
Ahmed Ammar <ahmed29329@gmail.com>
2024-05-12 02:19:48 +0200

    propper custom lsp setup

--- lua/ash/ash_lsp.lua
+++ lua/ash/ash_lsp.lua
@@ -1,15 +1,21 @@
 local util = require "lspconfig.util"
+local configs = require('lspconfig.configs')
+local lspconfig = require('lspconfig')
 
-require "lspconfig".phpactor.setup {
-    cmd = { "/Users/ahmed/go/src/ahmedash95/php-lsp-server/php-lsp-server" },
-    filetypes = { 'php' },
-    root_dir = function(pattern)
-        -- local cwd = vim.loop.cwd()
-        -- local root = util.root_pattern('composer.json', '.git')(pattern)
+configs.lsp_ash = {
+    default_config = {
+        cmd = { "/Users/ahmed/go/src/ahmedash95/php-lsp-server/php-lsp-server" },
+        filetypes = { 'php' },
+        root_dir = function(pattern)
+            -- local cwd = vim.loop.cwd()
+            -- local root = util.root_pattern('composer.json', '.git')(pattern)
 
-        local path = util.find_git_ancestor(pattern)
+            local path = util.find_git_ancestor(pattern)
 
-        return path
-    end,
+            return path
+        end,
+    }
 }
 
+
+lspconfig.lsp_ash.setup {}
