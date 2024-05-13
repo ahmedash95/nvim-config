30a460159d26397a8b953bada8a2ddbc40b614b2
Ahmed Ammar <ahmed.ammar@aboutyou.com>
2024-05-13 15:49:45 +0200

    run ash lsp if exists

--- lua/ash/ash_lsp.lua
+++ lua/ash/ash_lsp.lua
@@ -2,20 +2,32 @@ local util = require "lspconfig.util"
 local configs = require('lspconfig.configs')
 local lspconfig = require('lspconfig')
 
-configs.lsp_ash = {
-    default_config = {
-        cmd = { "/Users/ahmed/go/src/ahmedash95/php-lsp-server/php-lsp-server" },
-        filetypes = { 'php' },
-        root_dir = function(pattern)
-            -- local cwd = vim.loop.cwd()
-            -- local root = util.root_pattern('composer.json', '.git')(pattern)
+local function file_exists(name)
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else return false end
+end
 
-            local path = util.find_git_ancestor(pattern)
 
-            return path
-        end,
+local lspPath = "/Users/ahmed/go/src/ahmedash95/php-lsp-server/php-lsp-server"
+
+if file_exists(lspPath) then
+    configs.lsp_ash = {
+        default_config = {
+            cmd = { lspPath },
+            filetypes = { 'php' },
+            root_dir = function(pattern)
+                -- local cwd = vim.loop.cwd()
+                -- local root = util.root_pattern('composer.json', '.git')(pattern)
+
+                local path = util.find_git_ancestor(pattern)
+
+                return path
+            end,
+        }
     }
-}
 
-
-lspconfig.lsp_ash.setup {}
+    lspconfig.lsp_ash.setup {}
+end
