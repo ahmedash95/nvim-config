f27f1ac89a2db7f3f86584716e9503a402dcfb58
Ahmed Ammar <ahmed29329@gmail.com>
2024-05-19 18:09:56 +0200

    set line space between linenumber and code

--- lua/ash/init.lua
+++ lua/ash/init.lua
@@ -7,6 +7,9 @@ vim.opt.softtabstop = 4     -- Set the number of spaces tabs count for in insert
 vim.opt.shiftwidth = 4      -- Set the number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true    -- Use spaces instead of tabs
 vim.opt.scrolloff = 4       -- Keep 4 lines above and below the cursor when scrolling
+vim.cmd([[ set nofoldenable]]) -- Disable folding
+vim.o.statuscolumn = "%C%s%l  " -- set more space between the text and the line number
+
 
 require("ash.packages");
 require("ash.remap");
