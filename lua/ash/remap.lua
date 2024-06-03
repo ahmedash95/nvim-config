56898eb50b9aa364978226ebc1d0376085edc721
Ahmed Ammar <ahmed.ammar@aboutyou.com>
2024-06-03 15:17:06 +0200

    replace ctrl with leader
    Just to free ctrl to be used with zellij

--- lua/ash/remap.lua
+++ lua/ash/remap.lua
@@ -29,14 +29,14 @@ vim.keymap.set("n", "tq", ":tabclose<CR>", { noremap = true, silent = true })
 -- Telescope
 vim.keymap.set("n", "<leader>a", ":Telescope commands<CR>", { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>pf', ":Telescope find_files<CR>", { noremap = true, silent = true })
-vim.keymap.set('n', '<C-p>', ":Telescope git_files<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader><leader>', ":Telescope git_files<CR>", { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>b', ":Telescope buffers<CR>", {})
 vim.keymap.set('n', '<leader>ps', ":Telescope live_grep<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>1", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>2", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<C-o>", ":lua require(\"deepsymbols\").get_symbols()<CR>")
+vim.keymap.set("n", "<leader>o", ":lua require(\"deepsymbols\").get_symbols()<CR>")
 
 
 -- phpactor
