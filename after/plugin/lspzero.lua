b75b1c2e1dd9bbf012faa6c0b410f7e35c982a3b
Ahmed Ammar <ahmed29329@gmail.com>
2024-06-05 20:22:55 +0200

    lsp_zero cleanups

--- after/plugin/lspzero.lua
+++ after/plugin/lspzero.lua
@@ -1,13 +1,4 @@
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
-end)
-
 local cmp = require('cmp')
-local luasnip = require 'luasnip'
 
 cmp.setup({
     completion = {
