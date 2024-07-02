3c15d8cc51b773c3657e6d233ac1dccc7fe78a42
Ahmed Ammar <ahmed.ammar@aboutyou.com>
2024-07-02 16:35:27 +0200

    disable Transparency

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,11 +7,11 @@ auto_dark_mode.setup({
 	set_dark_mode = function()
 		vim.api.nvim_set_option('background', 'dark')
 		vim.cmd('colorscheme catppuccin-mocha')
-        vim.cmd('TransparentEnable')
+        -- vim.cmd('TransparentEnable')
 	end,
 	set_light_mode = function()
 		vim.api.nvim_set_option('background', 'light')
 		vim.cmd('colorscheme github_light')
-        vim.cmd('TransparentDisable')
+        -- vim.cmd('TransparentDisable')
 	end,
 })
