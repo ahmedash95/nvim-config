4052b51f959f415173daa0e6936ad0dc9958a4c0
Ahmed Ammar <ahmed.ammar@aboutyou.com>
2024-07-02 16:54:32 +0200

    set tokyonight as default dark theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,12 +1,10 @@
 local auto_dark_mode = require('auto-dark-mode')
 
-vim.cmd 'colorscheme github_light'
-
 auto_dark_mode.setup({
 	update_interval = 1000,
 	set_dark_mode = function()
 		vim.api.nvim_set_option('background', 'dark')
-		vim.cmd('colorscheme catppuccin-mocha')
+		vim.cmd('colorscheme tokyonight-moon')
         -- vim.cmd('TransparentEnable')
 	end,
 	set_light_mode = function()
