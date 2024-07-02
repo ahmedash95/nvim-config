3716c3a4810707212a947896ebe96576cf5474db
Ahmed Ammar <ahmed.ammar@aboutyou.com>
2024-07-02 17:08:17 +0200

    better nvim-tree setup

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -3,9 +3,23 @@ require("nvim-tree").setup({
         sorter = "case_sensitive",
     },
     view = {
-        width = 30,
+        width = 40,
+        adaptive_size = false,
+        auto_resize = false,
     },
     renderer = {
         group_empty = true,
     },
+    filters = {
+        dotfiles = false,
+        custom = {}, -- show ignored files
+    },
+    actions = {
+        open_file = {
+            resize_window = false, -- disaable resizing nvim-tree buffer when opneing new file
+        }
+    },
+    git = {
+        ignore = false, -- show git ignored files
+    }
 })
